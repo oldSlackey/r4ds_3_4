36362894e46a16a22f48c23424507f95607d16e8
oldSlackey <jules.augley@gmail.com>
2023-06-28 09:48:44 +0100

    chapter 4 added

---
+++ source/chapter4.R
@@ -0,0 +1,10 @@
+#' Chapter 4 of R for Data Science 2e
+#' https://r4ds.hadley.nz/
+#' 
+#' @author Jules Augley
+
+# 1. Setup ----
+library(here)
+here::i_am("source/chapter4.R")
+# not required for this chapter!
+# source(here::here("source", "dependencies.R"))
